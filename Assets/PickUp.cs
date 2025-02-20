15b39ab2e62b6e0e928061d4df2eae7681b969f1
popoyo07 <154477545+popoyo07@users.noreply.github.com>
2025-02-20 18:41:46 -0500

    fixed level

--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     //assume object has collider 
-    public GameObject guffin; //reference the pick up 
-    public Collider guffinCollider; //reference the pick up's collider, make sure the collider isTrigger
-    public Collider playerCollider; //reference the player's collider
-    public Renderer guffinRenderer; //reference the mesh renderer of guffin
-    void Start()
-    {
-        guffinCollider = guffin.GetComponent<Collider>();
-        guffinRenderer = guffin.GetComponent<Renderer>();
-    }
-
+    
+    
     
 
-    private void OnTriggerEnter(Collider playerCollider)
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("A collision has occured");
-        guffinRenderer.enabled = false;
+        if (other.CompareTag("item"))
+        {
+            Debug.Log("You collected an item");
+            Destroy(GameObject.Find("Item"));
+        }
+        
         
     }
 }
