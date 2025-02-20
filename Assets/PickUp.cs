629560bc18a95638dc116dad37c6ba3ee276dbd1
da413 <danielwolfcol@gmail.com>
2025-02-20 17:07:56 -0500

    pick up script

--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //assume object has collider 
+    public GameObject guffin; //reference the pick up 
+    public Collider guffinCollider; //reference the pick up's collider, make sure the collider isTrigger
+    public Collider playerCollider; //reference the player's collider
+    public Renderer guffinRenderer; //reference the mesh renderer of guffin
     void Start()
     {
-        
+        guffinCollider = guffin.GetComponent<Collider>();
+        guffinRenderer = guffin.GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    private void OnTriggerEnter(Collider playerCollider)
     {
+        Debug.Log("A collision has occured");
+        guffinRenderer.enabled = false;
         
     }
 }
